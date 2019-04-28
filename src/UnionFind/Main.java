154032eea32a709fc653cfeb49e1c20f82fcd20e
jinhwa <jinhwajinhwa@gmail.com>
2019-04-28 21:29:55 +0900

    quick find

---
+++ src/UnionFind/Main.java
@@ -0,0 +1,27 @@
+package UnionFind;
+
+import java.util.Scanner;
+
+public class Main extends QuickFindUF {
+
+    public Main(int N) {
+        super(N);
+    }
+
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        int N=scan.nextInt();
+        QuickFindUF uf=new QuickFindUF(N);
+        while (String.valueOf(N).equals(""))
+        {
+            int p= scan.nextInt();
+            int q= scan.nextInt();
+            if(!uf.connected(p,q))
+            {
+                uf.union(p,q);
+                System.out.println(p+" "+q);
+            }
+        }
+    }
+}
